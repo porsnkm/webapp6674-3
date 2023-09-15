b5d96849786f03fd7f24b237672416e835ed10b8
porsnkm <89564612+porsnkm@users.noreply.github.com>
2023-09-15 08:10:48 +0700

    Add files via upload

---
+++ ex01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    function test($str) {
+    $s = substr($str, -1);
+    return $s . $str . $s;
+}
+echo test("Red") . "\n";
+echo test("Green") . "\n";
+echo test("1") . "\n";
+?>
+</body>
+</html>
